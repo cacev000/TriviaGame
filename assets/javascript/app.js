2aaa6adadc41d950529b5ce6037fb80b739c6b3c
cacevedo <cacev000@gmail.com>
2019-01-11 16:26:46 -0500

    added the following reset values and restart game function, check answer with showAnswer function, show last page once done with trivia

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,20 +1,45 @@
 $(document).ready(function() {
     var queryUrl = 'https://opentdb.com/api.php?amount=8&category=11&type=multiple';
 
+    var startCounter;
     var triviaQuestions = [];
     var answerArray = [];
+    var correctResponse = '';
 
     var questionCounter = 0;
     var timeLeft = 30;
 
+    var extraText = $('#extraInfo');
+    var correctAnswerText = $('#correctAnswerText');
+
+    var totalCorrect = 0;
+    var totalIncorrect = 0;
+    var totalUnsolved = 0;
+
     // Using API to obtain 8 random questions with correct and incorrect answers
-    $.ajax(queryUrl).then(function(response) {
-        console.log(response.results);
-        setQuestionAndAnsers(response.results);
+    var retrieveTrivia = function() {
+        $('#startOver').hide();
+
+        $.ajax(queryUrl).then(function(response) {
+            console.log(response.results);
+            triviaQuestions = response.results;
+            setQuestionAndAnsers(response.results);
+        });
+    };
+
+    retrieveTrivia();
+
+    $('#startOver').click(function() {
+        resetValues();
+        retrieveTrivia();
     });
 
     function setQuestionAndAnsers(questionnaire) {
+        extraText.text('');
+        correctAnswerText.text('');
+
         // get array and retrieve data for each question. counter wil tell what question number we are on
+        correctResponse = questionnaire[questionCounter].correct_answer;
 
         // initialize array of answers
         answerArray = questionnaire[questionCounter].incorrect_answers;
@@ -41,16 +66,16 @@ $(document).ready(function() {
             // set the randomly selected answer to a given button depending on the current i value
             switch (i) {
                 case 0:
-                    $('#anwserOne').text(randomAnswer);
+                    jQuery('#anwserOne').html(randomAnswer).text();
                     break;
                 case 1:
-                    $('#anwserTwo').text(randomAnswer);
+                    jQuery('#anwserTwo').html(randomAnswer).text();
                     break;
                 case 2:
-                    $('#anwserThree').text(randomAnswer);
+                    jQuery('#anwserThree').html(randomAnswer).text();
                     break;
                 case 3:
-                    $('#anwserFour').text(randomAnswer);
+                    jQuery('#anwserFour').html(randomAnswer).text();
                     break;
 
                 default:
@@ -58,16 +83,105 @@ $(document).ready(function() {
             }
         }
         $('#countDown').text('Remaining Time: ' + timeLeft);
-        startCounter();
+        startCounter = setInterval(function() {
+            --timeLeft;
+            $('#countDown').text('Remaining Time: ' + timeLeft);
+
+            if (timeLeft <= 0) {
+                showAnswer('');
+                clearInterval(startCounter);
+            }
+        }, 1000);
     }
 
-    var startCounter = setInterval(function() {
-        --timeLeft;
-        $('#countDown').text('Remaining Time: ' + timeLeft);
+    $('#anwserOne').click(function() {
+        showAnswer($('#anwserOne').html());
+    });
 
-        if (timeLeft <= 0) {
-            console.log('reached 0');
-            clearInterval(startCounter);
+    $('#anwserTwo').click(function() {
+        showAnswer($('#anwserTwo').html());
+    });
+
+    $('#anwserThree').click(function() {
+        showAnswer($('#anwserThree').html());
+    });
+
+    $('#anwserFour').click(function() {
+        showAnswer($('#anwserFour').html());
+    });
+
+    function showAnswer(userResponse) {
+        // if timer ran out
+        if (timeLeft === 0 && userResponse === '') {
+            extraText.text('uhhhh, what happened? you ran out of time.');
+            jQuery(correctAnswerText).html('The correct answer was ' + correctResponse).text();
+            totalUnsolved++;
         }
-    }, 1000);
+
+        // if answer is correct it wil show Correct. if not it will show the correct answer
+        if (userResponse === correctResponse) {
+            extraText.text('CORRECT!');
+            extraText.css('color', 'green');
+            correctAnswerText.text('');
+            totalCorrect++;
+        } else {
+            extraText.text('NOPE!');
+            extraText.css('color', 'red');
+            jQuery(correctAnswerText).html('The correct answer was ' + correctResponse).text();
+            totalIncorrect++;
+        }
+
+        questionCounter++;
+
+        if (questionCounter < triviaQuestions.length) {
+            clearInterval(startCounter);
+
+            setTimeout(function() {
+                timeLeft = 30;
+                $('#countDown').text('Remaining Time: ' + timeLeft);
+                setQuestionAndAnsers(triviaQuestions);
+            }, 3000);
+        } else {
+            clearInterval(startCounter);
+            lastPage();
+        }
+    }
+
+    function lastPage() {
+        $('#question').hide();
+        $('#anwserOne').hide();
+        $('#anwserTwo').hide();
+        $('#anwserThree').hide();
+        $('#anwserFour').hide();
+
+        extraText.text('');
+        correctAnswerText.text('');
+
+        $('#anwsCorrect').text('Correct Answers: ' + totalCorrect);
+        $('#anwsCorrect').css('color', 'green');
+        $('#inCorrect').text('Incorrect Answers: ' + totalIncorrect);
+        $('#inCorrect').css('color', 'red');
+        $('#unSolved').text('Unsolved Answers: ' + totalUnsolved);
+
+        $('#startOver').show();
+    }
+
+    function resetValues() {
+        $('#question').show();
+        $('#anwserOne').show();
+        $('#anwserTwo').show();
+        $('#anwserThree').show();
+        $('#anwserFour').show();
+
+        $('#anwsCorrect').text('');
+        $('#inCorrect').text('');
+        $('#unSolved').text('');
+
+        questionCounter = 0;
+        totalCorrect = 0;
+        totalIncorrect = 0;
+        totalUnsolved = 0;
+
+        $('#startOver').hide();
+    }
 });
